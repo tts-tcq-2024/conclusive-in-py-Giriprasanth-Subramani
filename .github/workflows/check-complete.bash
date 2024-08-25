61c0fcbe9956338dfc681b57d0574588a5030663
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-08-25 21:48:41 +0530

    Update check-complete.bash

--- .github/workflows/check-complete.bash
+++ .github/workflows/check-complete.bash
@@ -1,5 +1,7 @@
-$'\r'
+@@ -1,5 +1,3 @@
+#!/bin/bash
+set -e
 if grep -q _enter *.md; then
-  echo "Replace all text having _enter with your input"
+  echo "Replace all text having Reflections"
   exit 1
-
+fi
